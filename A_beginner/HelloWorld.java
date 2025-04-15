fad25f4af51826b6964eec8fd01724f918f803f0
Faij21 <faijanshaikh27@gmail.com>
2025-04-15 11:46:30 +0530

    First Java commit

---
+++ A_beginner/HelloWorld.java
@@ -0,0 +1,8 @@
+package A_beginner;
+
+public class HelloWorld { // Declares a class named HelloWorld.
+
+    public static void main(String[] args) { // The main method is the entry point for Java applications.
+        System.out.println("Hello, World!"); // Prints the string to the console.
+    }
+}   
---
+++ C_advanced/testtwo.java
@@ -0,0 +1,8 @@
+package C_advanced;
+
+public class testtwo { // Declares a class named HelloWorld.
+
+    public static void main(String[] args) { // The main method is the entry point for Java applications.
+     
+    }
+}   
