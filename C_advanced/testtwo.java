3dbe8d62b7241fac1a6589449ea8645942b9a3bc
Faij21 <faijanshaikh27@gmail.com>
2025-04-15 18:16:12 +0530

    Swap two numbers

--- C_advanced/testtwo.java
+++
@@ -1,6 +0,0 @@
-public class testtwo { // Declares a class named HelloWorld.
-
-    public static void main(String[] args) { // The main method is the entry point for Java applications.
-     
-    }
-}   
